01fd8e556c26b70553124d7e195a76418b00e863
Roberto Henrique <roberto_sonicteam@hotmail.com>
2025-04-01 01:14:04 -0300

    Atividade concluída

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Exercício MilhasParaKm
+Receba uma medida em milhas e exiba seu equivalente em quilômetros. A medida em km é 1,609 vezes a medida em milhas.
+
+Entre com a medida (em milhas): 1
+1,609 Km
+*/
+
+double km = 1.609;
+Console.WriteLine("Entre com o total de milhas");
+double milhas = Convert.ToDouble(Console.ReadLine());
+double resultado = km * milhas;
+Console.WriteLine($"A medida convertida é de: {resultado} kilômetros");
+
+
